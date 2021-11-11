46a7b82cb2890b94cf15be061d3a7d86c33c6669
Jonathan Rojas <jonrojas.dev@gmail.com>
2021-11-11 02:20:26 -0800

    Render messages and location to html with Mustache

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,36 +1,86 @@
 //socket allows us to send an recieve events from the client
 const socket = io();
 
+//HTML Elements
+const messageForm = document.querySelector('#message-form');
+const messageFormInput = messageForm.querySelector('input');
+const messageFormButton = messageForm.querySelector('button');
+const sendLocationButton = document.querySelector('#send-location');
+const messages = document.querySelector('#messages');
+const coords = document.querySelector('#coords');
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-template').innerHTML;
+
+//Renders a users chat message to the console
 socket.on('message', (message) => {
   console.log(message);
+
+  const html = Mustache.render(messageTemplate, {
+    text: message
+  });
+  messages.insertAdjacentHTML('beforeend', html);
 });
 
-const form = document.getElementById('userMessage');
+//Listens for location data from server
+socket.on('locationMessage', (url) => {
+  const html = Mustache.render(locationMessageTemplate, {
+    coordinates: url
+  });
+  coords.insertAdjacentHTML('beforeend', html);
+});
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+//Event listener for when a user submits their message
+messageForm.addEventListener('submit', (e) => {
   //Prevents the page from refreshing when form is submitted
   e.preventDefault();
+
+  //disables submit button after sending to prevent spam sends
+  messageFormButton.setAttribute('disabled', 'disabled');
 
   //User input saved inside a const
   //target represents the target we are listening for the event on which is the form
   //elements represents the inputs within the form
   const message = e.target.elements.userMessage.value;
 
-  socket.emit('sendMessage', message);
+  socket.emit('sendMessage', message, (error) => {
+    //enables button once message has been sent to server
+    messageFormButton.removeAttribute('disabled');
+    //Clears input once message has been sent
+    messageFormInput.value = '';
+    //Moves focus back to text box input once message has been sent
+    messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message delivered!');
+  });
 });
 
-document.querySelector('#send-location').addEventListener('click', (e) => {
-  // console.log(Geolocation.getCurrentPosition());
-  // console.log('Hello');
+//Event listener for when user wants to send location
+sendLocationButton.addEventListener('click', (e) => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser');
   }
+  //disabling the weather button after being clicked
+  sendLocationButton.setAttribute('disabled', 'disabled');
+
   navigator.geolocation.getCurrentPosition((position) => {
     const userLocation = {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     };
-    //console.log(position.coords.longitude);
-    socket.emit('sendLocation', userLocation);
+    //With coordinates fetched, we send this data back to server
+    socket.emit('sendLocation', userLocation, () => {
+      //enabling button
+      //sendLocationButton.removeAttribute('disabled');
+
+      //Sending event confirmation to console confirming everthing went fine
+      console.log('Location shared!');
+    });
   });
 });
+
+// const form = document.getElementById('userMessage');
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -27,20 +28,28 @@ io.on('connection', (socket) => {
   //broadcast is used to emit a message to all users EXCEPT the person sending the message
   socket.broadcast.emit('message', 'A new user has joined!!!');
 
-  //on is used to recieve the event sent form the client
-  //emits to the person updating the count in real time
-
   //io.emit sends to all users vs sokcet.io which is only visible to user that sent message
   //This sends the updated count back to ALL clients
-  socket.on('sendMessage', (userMessage) => {
-    //The userMessage is then emitted back to the client via the 'message' event
+  socket.on('sendMessage', (userMessage, callback) => {
+    //loading in the filter function and passing in the message to check for bad words
+    const filter = new Filter();
+    if (filter.isProfane(userMessage)) {
+      return callback('Be kind, please dont swear.');
+    }
+
+    //The userMessage is then emitted back to the client via the 'message' event if no bad lang was found
     io.emit('message', userMessage);
+    callback();
   });
 
+  //Renders a link when user shares location
+  // socket.emit('locationMessage', url);
+
   //Listens for share location button
-  socket.on('sendLocation', (userCoordinates) => {
-    //https://www.google.com/maps/?=q=0,0
-    io.emit('message', 'https://www.google.com/maps/?=q=' + userCoordinates.latitude + ',' + userCoordinates.longitude);
+  socket.on('sendLocation', (userCoordinates, callback) => {
+    socket.emit('locationMessage', 'https://www.google.com/maps/?=q=' + userCoordinates.latitude + ',' + userCoordinates.longitude);
+    //io.emit('message', 'https://www.google.com/maps/?=q=' + userCoordinates.latitude + ',' + userCoordinates.longitude);
+    callback();
   });
 
   //Code that runs whenver a user disconnects i.e closing their browser
